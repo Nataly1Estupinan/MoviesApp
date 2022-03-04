235af351dd4fabffb2121b705e206502f91ed702
Nataly <naesdu@gmail.com>
2022-03-04 11:58:22 -0500

    no -unused vars

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-
 import { MovieCard } from "./MovieCard";
 import { get } from "../utils/httpClient";
 import styles from "./MoviesGrid.module.css";
@@ -11,31 +10,28 @@ export function MoviesGrid({search}) {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
-  const [hasMore, setHasMore]= useState(true);
-
- 
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
     setIsLoading(true);
-
     const searchUrl = search
       ? "/search/movie?query=" + search + "&page=" + page
       : "/discover/movie?page=" + page;
     get(searchUrl).then((data) => {
       setMovies((prevMovies) => prevMovies.concat(data.results));
-      setHasMore(data.page < data.total_pages)
+      setHasMore(data.page < data.total_pages);
       setIsLoading(false);
     });
   }, [search, page]);
 
-  if(!isLoading && movies.length === 0){
-    return <Empty/>;
+  if (!isLoading && movies.length === 0) {
+    return <Empty />;
   }
 
   return (
     <InfiniteScroll
       dataLength={movies.length}
-      hasMore={true}
+      hasMore={hasMore}
       next={() => setPage((prevPage) => prevPage + 1)}
       loader={<Spinner />}
     >
